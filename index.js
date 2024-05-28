4710b02ddafea061908754d1251a9cc41fb56169
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-05-28 20:18:59 +0530

    misc

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const mainRouter = require("./routes/index");
 const cors = require("cors");
 require("dotenv").config();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
-app.use(
-    cors({
-        origin: "https://payment-wallet-kv.vercel.app",
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: "https://payment-wallet-kv.vercel.app",
+//         credentials: true,
+//     })
+// );
 
 dbConnect();
 
